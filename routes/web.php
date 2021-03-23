6eae6410a73436aadec66471adb8846d0a947b4e
Matt Sievers <fantsigns@gmail.com>
2021-03-23 17:54:03 -0500

    Activity 2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    echo "Hello World";
+});
+
+Route::get('/helloworld', function(){
+   return view ('helloworld');
+});
+
+Route::get('/test', 'TestController@test2');
+
+Route::get('/user', 'UserController@foo');
+
+Route::post('/whoami', 'WhatsMyNameController@index');
+
+Route::get('/askme', function (){
+    return view('whoami');
+});
+
+Route::get('/login', function (){
+    return view('login');
+});
+
+Route::post('/dologin', 'LoginController@index');
+
+Route::get('/login2', function (){
+    return view('login2');
+});
